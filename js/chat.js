f1d2475ab0e4eb14882f18d004822643bfaf38f6
Heo <gjgudrn1114@naver.com>
2023-08-29 19:50:24 +0900

    리팩토링

--- js/chat.js
+++ js/chat.js
@@ -1,3 +1,10 @@
+const eventSource = new EventSource("http://localhost:8080/sender/a/receiver/b");
+
+eventSource.onmessage = (event) => {
+    const data = JSON.parse(event.data);
+    console.log(2, data);
+}
+
 function getSendMsgBox(msg) {
     return `<div class="sent_msg">
     <p>${msg}</p>
@@ -5,8 +12,7 @@ function getSendMsgBox(msg) {
   </div>`;
 }
 
-
-document.querySelector("#chat-send").addEventListener("click", () => {
+function addMessage() {
     let chatBox = document.querySelector("#chat-box");
     let msgInput = document.querySelector("#chat-outgoing-msg");
 
@@ -15,17 +21,15 @@ document.querySelector("#chat-send").addEventListener("click", () => {
     chatOutgoingBox.innerHTML = getSendMsgBox(msgInput.value);
     chatBox.append(chatOutgoingBox);
     msgInput.value = "";
+}
+
+
+document.querySelector("#chat-send").addEventListener("click", () => {
+    addMessage();
 });
 
 document.querySelector("#chat-outgoing-msg").addEventListener("keydown", (e) => {
     if (e.keyCode === 13) {
-        let chatBox = document.querySelector("#chat-box");
-        let msgInput = document.querySelector("#chat-outgoing-msg");
-    
-        let chatOutgoingBox = document.createElement("div");
-        chatOutgoingBox.className = "outgoing_msg";
-        chatOutgoingBox.innerHTML = getSendMsgBox(msgInput.value);
-        chatBox.append(chatOutgoingBox);
-        msgInput.value = "";
+        addMessage();
     }
 })
