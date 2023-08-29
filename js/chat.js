860b6e5644b04935deeef7924b91de5ccfcaf61e
Heo <gjgudrn1114@naver.com>
2023-08-29 20:26:35 +0900

    기존 채팅 내역 조회 기능 추가

--- js/chat.js
+++ js/chat.js
@@ -2,14 +2,38 @@ const eventSource = new EventSource("http://localhost:8080/sender/a/receiver/b")
 
 eventSource.onmessage = (event) => {
     const data = JSON.parse(event.data);
-    console.log(2, data);
+
+    const parsed_year = data.createdAt.substr(0, 4);
+    const parsed_month = data.createdAt.substr(5, 2);
+    const parsed_day = data.createdAt.substr(8, 2);
+    const parsed_hour = data.createdAt.substr(11, 2);
+    const parsed_minute = data.createdAt.substr(14, 2);
+    const parsed_second = data.createdAt.substr(17, 2);
+
+
+    const parsed_time = parsed_year + '-' + parsed_month + '-' + parsed_day
+        + ' | ' + parsed_hour + ':' + parsed_minute + ':' + parsed_second;
+
+    console.log(parsed_time);
+    initMessage(data, parsed_time);
 }
 
-function getSendMsgBox(msg) {
+function getSendMsgBox(msg, time) {
     return `<div class="sent_msg">
     <p>${msg}</p>
-    <span class="time_date"> 11:18 | Today</span>
+    <span class="time_date"> ${time} </span>
   </div>`;
+}
+
+function initMessage(data, time) {
+    let chatBox = document.querySelector("#chat-box");
+    let msgInput = document.querySelector("#chat-outgoing-msg");
+
+    let chatOutgoingBox = document.createElement("div");
+    chatOutgoingBox.className = "outgoing_msg";
+    chatOutgoingBox.innerHTML = getSendMsgBox(data.msg, time);
+    chatBox.append(chatOutgoingBox);
+    msgInput.value = "";
 }
 
 function addMessage() {
@@ -18,7 +42,21 @@ function addMessage() {
 
     let chatOutgoingBox = document.createElement("div");
     chatOutgoingBox.className = "outgoing_msg";
-    chatOutgoingBox.innerHTML = getSendMsgBox(msgInput.value);
+
+    let date = new Date();
+
+    let year = date.getFullYear();
+    let month = ('0' + (date.getMonth() + 1)).slice(-2);
+    let day = ('0' + date.getDate()).slice(-2);
+    let hours = ('0' + date.getHours()).slice(-2);
+    let minutes = ('0' + date.getMinutes()).slice(-2);
+    
+    let seconds = ('0' + date.getSeconds()).slice(-2);
+
+    let now = year + '-' + month + '-' + day
+        + ' | ' + hours + ':' + minutes + ':' + seconds;
+
+    chatOutgoingBox.innerHTML = getSendMsgBox(msgInput.value, now);
     chatBox.append(chatOutgoingBox);
     msgInput.value = "";
 }
